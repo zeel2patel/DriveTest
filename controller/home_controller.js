6232091195fd36f945bd514cccdf7246c90d777b
Zeel Patel <pzeel1330@gmail.com>
2023-08-11 16:22:53 -0400

    delete

--- controller/home_controller.js
+++
@@ -1,5 +0,0 @@
-const homePage = (req, res) => {
-  res.render("index.ejs");
-};
-
-module.exports = { homePage };
--- middleware/validate_error.js
+++
@@ -1,8 +0,0 @@
-
-const extractValidationErrors = (err) => {
-    const validationErrors = Object.values(err.errors).map((error) => error.message);
-    return validationErrors;
-  };
-  
-module.exports = extractValidationErrors;
-  
