c3bf1356f78175d91a84c60f230a42a641162cfd
Zeel Patel <pzeel1330@gmail.com>
2023-12-21 02:28:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const express = require("express");
-const app = express();
-const port = 4050;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const expressSession = require("express-session");
